94b2a1337fccb5acd7713cc451b0c46b27335edc
Jorge Langa <jorge.langa.arranz@gmail.com>
2017-07-21 12:29:21 +0200

    Bugfix when indexing crams and fais

--- src/snakefiles/generic.py
+++ src/snakefiles/generic.py
@@ -1,19 +1,19 @@
 rule index_bam:
     input: "{filename}.bam"
     output: "{filename}.bam.bai"
-    shell: "samtools index {input} {output}"
+    shell: "samtools index {input}"
 
 rule index_cram:
     input: "{filename}.cram"
     output: "{filename}.cram.crai"
-    shell: "samtools index {input} {output}"
+    shell: "samtools index {input}"
 
 rule index_fasta:
     input: "{filename}.fasta"
     output: "{filename}.fasta.fai"
-    shell: "samtools faidx {input} {output}"
+    shell: "samtools faidx {input}"
 
 rule index_fa:
     input: "{filename}.fa"
     output: "{filename}.fa.fai"
-    shell: "samtools faidx {input} {output}"
+    shell: "samtools faidx {input}"
