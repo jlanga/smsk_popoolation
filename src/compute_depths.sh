ba1f79bb82acd876a420b41ef00094f0685c0fb6
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-07-02 09:28:54 +0200

    Update snakemake to use pipes. Linted compute_depths.sh

--- src/compute_depths.sh
+++ src/compute_depths.sh
@@ -4,13 +4,13 @@ set -euxo pipefail
 mkdir -p depths
 
 populations=(ada_grc ana_tur arm_arm car_aut car_svn_hrv cau_tur cec_grc cyp_cyp ibe_esp_eus ibe_esp_north ibe_esp_south ibe_esp_west lig_ita mac_bgr mac_mkd_grc mac_rou_mda mel_che mel_dnk mel_imn mel_irl mel_rus rut_mlt)
-for population in ${populations[@]}; do
-    (samtools merge -u - results/map/filt/${population}.*.{1..16}.cram \
+for population in "${populations[@]}"; do
+    (samtools merge -u - results/map/filt/"${population}".*.{1..16}.cram \
     | samtools depth -a - \
     | cut -f 3 \
     | sort -n \
     | uniq -c \
     | awk '{print $2"\t"$1}' \
-    > depths/$population.depths.tsv) &
+    > depths/"$population".depths.tsv) &
 done; wait
 
